2ec6456900b872fb942853b2f4872cfe8f0991f7
berko <dberkowitz@berkpc.com>
2013-05-14 12:14:48 -0700

    Initial coit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '95988572bcff6327910ec7c3f438f95273cce7a1437c023bfecdb641d45d606d9601da38a6a56704078005489bf07ede7ca35913e8ab8287af9368ff3d0c30a1'
